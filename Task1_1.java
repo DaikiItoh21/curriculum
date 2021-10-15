25e8ee5f935df99d94306c31dffffd2b1b1bd20e
tensainoisiwotugumono <redaiki.itoh@gmail.com>
2021-10-15 10:32:59 +0900

    Create Task1_1.java

---
+++ Task1_1.java
@@ -0,0 +1,18 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // [hogeDoubleに変数0.2を代入している]
+        double hogeDouble = 0.2;
+        hogeInt = 100;
+        hogeString = "合格";
+        System.out.println(hogeInt);
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // [変数hogeStringの値を表示している]
+        System.out.println(hogeString);
+        System.out.println(hogeDouble);
+
+    }
+}
